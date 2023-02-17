93d50de9b3a46db1745fedf1b55f114cf324226e
RodriguesFundo <rodriguesdomingosf@gmail.com>
2023-02-16 22:24:22 -0800

    Todas Model ja foram criadas

--- My Notes/My Notes/UserRegisterViewModel.cs
+++ My Notes/My Notes/UserRegisterViewModel.cs
@@ -1,57 +1,10 @@
-﻿using My_Notes.Models;
-using SQLite;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace My_Notes
 {
-    public class UserRegisterViewModel
+    internal class UserRegisterViewModel
     {
-        SQLiteConnection sQLiteConnection;
-        public string StatusBD { get; set; }
-        public UserRegisterViewModel(string dbPath)
-        {
-            if (dbPath=="") dbPath=App.DBPath; //Se nao encontrar o local, atribua o valor da variavel global
-            sQLiteConnection = new SQLiteConnection(dbPath); //Definicao da BD
-            sQLiteConnection.CreateTable<User>();
-
-        }
-
-        //Insercao
-        public void InserirUser(User user) {
-            try {
-                if (string.IsNullOrEmpty(user.Nome))
-                    throw new Exception("Campo nome vazio");
-                if (string.IsNullOrEmpty(user.Email))
-                    throw new Exception("Campo email vazio");
-                if (string.IsNullOrEmpty(user.Password))
-                    throw new Exception("Campo senha vazio");
-
-                var result = sQLiteConnection.Insert(user);
-                if (result != 0) 
-                {
-                    this.StatusBD = $"Usuario {user.Nome} adicionado";
-                }
-                else
-                {
-                    this.StatusBD = $"Usuario nao adicionado";
-                }
-
-            }
-            catch (Exception ex) 
-            {
-                throw new Exception(ex.Message.ToString());
-            }
-            finally
-            {
-                sQLiteConnection.Close();
-            }
-        }
-
-
-    
-        
-
     }
 }
